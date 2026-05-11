1671b3aeb2205dca33f2be2e75ee43eb8f23207e
Kurt Friars <kfriars@apple.com>
2026-05-11 14:02:56 -0400

    master branch to support laravel 13.x only

--- tests/ObservableEventsTest.php
+++ tests/ObservableEventsTest.php
@@ -1,21 +1,10 @@
 <?php
 
-namespace Plank\LaravelPivotEvents\Tests;
-
 use Plank\LaravelPivotEvents\Tests\Models\User;
 
-class ObservableEventsTest extends TestCase
-{
-    protected function setUp(): void
-    {
-        parent::setUp();
-    }
+test('observable events include pivot events', function () {
+    $user = User::find(1);
+    $events = $user->getObservableEvents();
 
-    public function test_events()
-    {
-        $user = User::find(1);
-        $events = $user->getObservableEvents();
-
-        $this->assertTrue(in_array('pivotAttaching', $events));
-    }
-}
+    expect($events)->toContain('pivotAttaching');
+});
--- tests/TestCase.php
+++ tests/TestCase.php
@@ -50,19 +50,18 @@ abstract class TestCase extends \Orchestra\Testbench\TestCase
         });
     }
 
+    protected function defineDatabaseMigrations()
+    {
+        $this->loadMigrationsFrom(__DIR__.'/database/migrations');
+    }
+
     protected function getEnvironmentSetUp($app)
     {
-        // Setup default database to use sqlite :memory:
         $app['config']->set('database.default', 'testbench');
         $app['config']->set('database.connections.testbench', [
             'driver' => 'sqlite',
             'database' => ':memory:',
             'prefix' => '',
         ]);
-    }
-
-    protected function getPackageProviders($app)
-    {
-        return [ServiceProvider::class];
     }
 }
